144c78ec259f80842ac34a633be5841f0750e776
Harrison Wilco <hwilco@gmail.com>
2021-12-01 01:38:52 -0500

    Updated Card tests for color attribute.

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -9,26 +9,26 @@ class TestCard(unittest.TestCase):
     def test_value_outside_range(self):
         for value in [0, 14]:
             with self.subTest(value=value):
-                self.assertRaises(ValueError, deck.Card, value, 'h')
+                self.assertRaises(ValueError, deck.Card, value, 'h', 'blue')
 
     def test_value_inside_range(self):
         for value in range(1, 14):
             with self.subTest(msg="ValueError unexpectedly raised by Card({}, 'h')".format(value), value=value):
                 try:
-                    deck.Card(value, 'h')
+                    deck.Card(value, 'h', 'blue')
                 except ValueError:
                     self.fail()
 
     def test_suit_invalid(self):
         for suit in ['clubs', 'diamonds', 'hearts', 'spades'] + [s for s in string.ascii_lowercase if s not in 'cdhs']:
             with self.subTest(suit=suit):
-                self.assertRaises(ValueError, deck.Card, 1, suit)
+                self.assertRaises(ValueError, deck.Card, 1, suit, 'blue')
 
     def test_suit_valid(self):
         for suit in 'cdhsCDHS':
             with self.subTest(msg="ValueError unexpectedly raised by Card(1, {})".format(suit), suit=suit):
                 try:
-                    deck.Card(1, suit)
+                    deck.Card(1, suit, 'blue')
                 except ValueError:
                     self.fail()
 
