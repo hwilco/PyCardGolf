dde591d2a041a957e7fb87ffb86e585df31c9b28
Harrison Wilco <hwilco@gmail.com>
2021-12-04 18:25:41 -0500

    Generate 5 random cards in setUpClass() for TestCardStack.

--- tests/test_deck.py
+++ tests/test_deck.py
@@ -14,50 +14,49 @@ def random_cards(num_cards: int):
 
 
 class TestCardStack(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls) -> None:
+        cls.cards_5 = random_cards(5)
+
     def test_add_card_stack(self):
-        cards = random_cards(5)
-        card_stack = deck.CardStack(cards=cards.copy())
+        card_stack = deck.CardStack(cards=self.cards_5.copy())
         other_cards = random_cards(3)
         other_card_stack = deck.CardStack(cards=other_cards.copy())
         card_stack.add_card_stack(other_card_stack)
-        self.assertEqual(card_stack._cards, cards + other_cards)
+        self.assertEqual(card_stack._cards, self.cards_5 + other_cards)
         self.assertEqual(other_card_stack.num_cards, 0)
 
     def test_add_card_stack_no_clear(self):
-        cards = random_cards(5)
-        card_stack = deck.CardStack(cards=cards.copy())
+        card_stack = deck.CardStack(cards=self.cards_5.copy())
         other_cards = random_cards(3)
         other_card_stack = deck.CardStack(cards=other_cards.copy())
         card_stack.add_card_stack(other_card_stack, clear_other=False)
-        self.assertEqual(card_stack._cards, cards + other_cards)
+        self.assertEqual(card_stack._cards, self.cards_5 + other_cards)
         self.assertEqual(other_card_stack._cards, other_cards)
 
     def test_add_card_stack_shuffle(self):
-        cards = random_cards(5)
-        card_stack = deck.CardStack(cards=cards.copy())
+        card_stack = deck.CardStack(cards=self.cards_5.copy())
         other_cards = random_cards(3)
         other_card_stack = deck.CardStack(cards=other_cards.copy())
         card_stack.rand = mock.MagicMock()
         card_stack.add_card_stack(other_card_stack, shuffle=True)
-        card_stack.rand.shuffle.assert_called_once_with(cards + other_cards)
-        self.assertEqual(card_stack._cards, cards + other_cards)
+        card_stack.rand.shuffle.assert_called_once_with(self.cards_5 + other_cards)
+        self.assertEqual(card_stack._cards, self.cards_5 + other_cards)
         self.assertEqual(other_card_stack.num_cards, 0)
 
     def test_draw(self):
-        cards = random_cards(5)
-        card_stack = deck.CardStack(cards=cards.copy())
-        for num_draws in range(1, len(cards) + 1):
+        card_stack = deck.CardStack(cards=self.cards_5.copy())
+        for num_draws in range(1, len(self.cards_5) + 1):
             with self.subTest(num_draws=num_draws):
-                self.assertEqual(card_stack.draw(), cards[-num_draws])
+                self.assertEqual(card_stack.draw(), self.cards_5[-num_draws])
 
     def test_draw_empty(self):
         card_stack = deck.CardStack()
         self.assertRaises(IndexError, card_stack.draw)
 
     def test_clear(self):
-        num_cards = 2
-        card_stack = deck.CardStack(cards=random_cards(num_cards))
-        self.assertEqual(card_stack.num_cards, num_cards)
+        card_stack = deck.CardStack(cards=self.cards_5.copy())
+        self.assertEqual(card_stack.num_cards, len(self.cards_5))
         card_stack.clear()
         self.assertEqual(card_stack.num_cards, 0)
         card_stack.clear()
@@ -81,14 +80,24 @@ class TestCardStack(unittest.TestCase):
             self.assertEqual(card_stack._cards, comp_cards)
 
     def test_eq(self):
-        cards = random_cards(4)
         self.assertEqual(deck.CardStack(seed=1), deck.CardStack(seed=1))
-        self.assertEqual(deck.CardStack(seed=1, cards=cards.copy()), deck.CardStack(seed=1, cards=cards.copy()))
+        self.assertEqual(deck.CardStack(seed=1, cards=self.cards_5.copy()), deck.CardStack(seed=1, cards=self.cards_5
+                                                                                           .copy()))
 
         self.assertNotEqual(deck.CardStack(seed=1), deck.CardStack(seed=2))
-        self.assertNotEqual(deck.CardStack(seed=1, cards=cards.copy()), deck.CardStack(seed=2, cards=cards.copy()))
-        self.assertNotEqual(deck.CardStack(seed=1, cards=cards.copy()), deck.CardStack(seed=1))
-        self.assertNotEqual(deck.CardStack(seed=1, cards=cards.copy()), deck.CardStack(seed=1, cards=cards.copy()[:2]))
+        self.assertNotEqual(deck.CardStack(seed=1, cards=self.cards_5.copy()), deck.CardStack(seed=2, cards=self.cards_5
+                                                                                              .copy()))
+        self.assertNotEqual(deck.CardStack(seed=1, cards=self.cards_5.copy()), deck.CardStack(seed=1))
+        self.assertNotEqual(deck.CardStack(seed=1, cards=self.cards_5.copy()), deck.CardStack(seed=1, cards=self.cards_5
+                                                                                              .copy()[:2]))
+
+    def test_str(self):
+        self.fail()
+
+    def test_repr(self):
+        self.assertEqual(repr(deck.CardStack(seed=1)), "CardStack(cards=[], seed=1)")
+        self.assertEqual(repr(deck.CardStack(cards=self.cards_5.copy(), seed=1)), "CardStack(cards={}, seed=1)".format(
+            self.cards_5))
 
 
 class TestDeck(unittest.TestCase):
