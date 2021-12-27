9d09ad309ab52bde2b734facbd71083a3b1cff14
Harrison Wilco <hwilco@gmail.com>
2021-12-27 18:48:16 -0500

    Add support for Suit comparison and __contains__ method.

--- src/pycardgolf/utils/const.py
+++ src/pycardgolf/utils/const.py
@@ -1,8 +1,19 @@
-from enum import Enum
+from enum import EnumMeta, Enum
 
 
-class Suit(Enum):
+class _ContainsEnumMeta(EnumMeta):
+    def __contains__(self, item):
+        return item in self.__members__.values()
+
+
+class Suit(Enum, metaclass=_ContainsEnumMeta):
     CLUBS = 0
     DIAMONDS = 1
     HEARTS = 2
     SPADES = 3
+
+    def __lt__(self, other: 'Suit'):
+        return self.value < other.value
+
+
+__all__ = ['Suit']
